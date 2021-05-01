fba2923dc980d2f1bc95303bef5929a496c6a9a1
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-01 14:30:36 -0500

    initial commit

---
+++ src/main/java/com/plantsio/PlantsioApplication.java
@@ -0,0 +1,13 @@
+package com.plantsio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantsioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plantsio/PlantsioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantsio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
